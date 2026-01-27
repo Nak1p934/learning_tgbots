9fc78589b997519114e7cc2a7b01f0c574676a27
Nak1p934 <leonleon0308@gmail.com>
2026-01-27 14:32:44 +0300

    ry fix bug bot dont work

--- lesson1/run.py
+++ lesson1/run.py
@@ -4,15 +4,15 @@ import asyncio
 from routes_for_lesson1 import router_less1
 
 
-async def main():
-    dp = Dispatcher()
-    bot = Bot(token=TOKEN1)
-    dp.include_router(router_less1)
-    await dp.start_polling(bot)
+async def main_less1():
+    dp_less1 = Dispatcher()
+    bot_less1 = Bot(token=TOKEN1)
+    dp_less1.include_router(router_less1)
+    await dp_less1.start_polling(bot_less1)
 
 
 if __name__ == "__main__":
     try:
-        asyncio.run(main()) 
+        asyncio.run(main_less1()) 
     except:
         print("Завершение работы")
